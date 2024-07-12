bd5bfd3771ed6e4ab0265afe03ad3bafc10018e4
Myo Thu Kha <myothukha.dev@gmail.com>
2024-07-12 23:55:13 +0700

    [add] mock data and images

--- AppleFrameworks/FrameworkGridView.swift
+++ AppleFrameworks/FrameworkGridView.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        FrameworkTileView()
+        
     }
 }
 
 #Preview {
     FrameworkGridView()
+}
+
+
+struct FrameworkTileView: View{
+    
+    var body: some View{
+        Text("Hello world")
+    }
+    
 }
