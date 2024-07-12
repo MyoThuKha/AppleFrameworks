2de7ddf90e9880be2359e81cd996bc895249cc22
Myo Thu Kha <myothukha.dev@gmail.com>
2024-07-12 21:10:30 +0700

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Myo Thu Kha on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
